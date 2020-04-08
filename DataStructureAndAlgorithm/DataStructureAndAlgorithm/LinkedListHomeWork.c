677da9086438022348514a84b2f9596b9a8c9820
Apparitionqk <apparitionqk.sina.cn>
2020-04-08 18:18:07 +0800

    链表测试题

---
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/LinkedListHomeWork.c
@@ -0,0 +1,14 @@
+//
+//  LinkedListHomeWork.c
+//  DataStructureAndAlgorithm
+//
+//  Created by 123 on 2020/4/8.
+//  Copyright © 2020 齐科. All rights reserved.
+//
+
+#include "LinkedListHomeWork.h"
+
+Status mergeTwoList(void) {
+    
+    return OK;
+}
---
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/LinkedListHomeWork.h
@@ -0,0 +1,20 @@
+//
+//  LinkedListHomeWork.h
+//  DataStructureAndAlgorithm
+//
+//  Created by 123 on 2020/4/8.
+//  Copyright © 2020 齐科. All rights reserved.
+//
+
+#ifndef LinkedListHomeWork_h
+#define LinkedListHomeWork_h
+
+#include <stdio.h>
+#include "Macros.h"
+
+/*
+    将两个递增的有序链表合并为一个有序的链表
+    要求：结果链表仍然使用两个链表的存储空间，不占用其他空间，表中不允许有重复的数据
+*/
+Status mergeTwoList(void);
+#endif /* LinkedListHomeWork_h */
